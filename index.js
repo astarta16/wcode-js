62fc3a7fde4af37466b90fbf5ce67fd87f4a5873
astarta16 <ngedeshuri2001@gmail.com>
2024-07-06 11:47:26 +0400

    add second soldev problem

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // N1
 function sum(numbers) {
-    let total = 0;
-    for (let number of numbers) {
-        total += number;
-    }
-    return total;
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return total;
 }
 
 let numbers = [10, 50, 6, 7, 8, 11, 6, 3, 9];
 let result = sum(numbers);
-console.log("Sum of this numbers is", + result);
+console.log("Sum of this numbers is", +result);
+
+// N2
+
+function getFullName(user) {
+  if (user.isloggedin) {
+    return `${user.firstname} ${user.lastname}`;
+  } else {
+    return false;
+  }
+}
+
+let user = {
+  firstname: "giorgi",
+  lastname: "saakadze",
+  age: 32,
+  isloggedin: true,
+};
+
+console.log(getFullName(user));
